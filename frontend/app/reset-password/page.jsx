cf478fe33e00eac360cc4731c9d463eaf4ace563
Pranjal Patel <Pranjal.p@dev.rkitsoft.local>
2026-03-02 12:49:27 +0530

    reset-password page updated!!

--- frontend/app/reset-password/page.jsx
+++ frontend/app/reset-password/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Suspense } from "react";
-import ResetPasswordContent from "./ResetPasswordContent";
+import ResetPasswordContent from "../reset-password/resetPasswordContent";
 
 export default function ResetPasswordPage() {
   return (
